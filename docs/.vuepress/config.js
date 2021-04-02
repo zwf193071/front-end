01d8a42f004a716e4e37c1cd86b614f270779beb
01403558 <zhuwenfang@sfmail.sf-express.com>
2021-04-02 13:30:38 +0800

    create blog website

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,68 @@
+module.exports = {
+    title: '前端技术文档',
+    description: '说明文档',
+    base: '/front-end/',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                vue$: 'vue/dist/vue.esm.js',
+                // "@": path.join(__dirname)
+            },
+        },
+    },
+    head: [
+        [
+            'link',
+            {
+                rel: 'icon',
+                href: `/logo.png`,
+            },
+        ],
+    ],
+    theme: 'reco',
+    themeConfig: {
+        repo: 'https://github.com/zwf193071/front-end',
+        repoLabel: 'My Blog',
+        nav: [
+            {text: '主页', link: '/'},
+            {
+                text: '前端技术',
+                link: '/algorithm/',
+                items: [
+                    // {text: 'ES6', link: '/JS/ES6'},
+                    // {text: 'Cesium', link: '/JS/Cesium'},
+                    // {text: '前端代码规范', link: '/JS/CodeStandards'},
+                    // {text: 'vscode插件推荐', link: '/JS/VSCodePlugins'},
+                ],
+            }
+        ],
+        sidebar: {
+            '/algorithm/': [
+                {
+                    title: '前端技术',
+                    collapsable: false,
+                },
+            ]
+        },
+        // 内置搜索
+        search: true,
+        searchMaxSuggestions: 10,
+        // 最后更新时间
+        lastUpdated: '最后更新时间', // string | boolean
+        mode: 'light', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
+        modePicker: false, // 默认 true，false 不显示模式调节按钮，true 则显示
+        /**
+         * support for
+         * 'default'
+         * 'funky'
+         * 'okaidia'
+         * 'solarizedlight'
+         * 'tomorrow'
+         */
+        codeTheme: 'tomorrow', // default 'tomorrow'
+        subSidebar: 'auto', //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
+    },
+    plugins: [['@vuepress/back-to-top', true],
+
+    ]
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroText: 前端技术
+heroImage: /logo.png
+tagline: 说明文档
+actionText: 立即开始
+actionLink: /algorithm
+features:
+  - title: Vue
+    details: Vue双向绑定数据的原理是什么？Vue3解决了哪些问题...
+  - title: Cesium
+    details: 如何利用Cesium构建三维模型？Cesium与vue如何结合...
+  - title: 技术分享 
+    details: 交流技术，分享经验，后期将会引入评论功能，方便大家在每一篇文章下留下自己的评论.
+footer: MIT Licensed | Copyright © 2021-present
+---
